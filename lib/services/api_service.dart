17b8be4844c7073f258470448c6de4f898c136a8
financeiro642 <financeiro@tlconsultorias.com.br>
2026-07-08 11:51:18 -0300

    Update api_service.dart

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -4,7 +4,7 @@ import '../models/camera.dart';
 import '../models/event_item.dart';
 
 class ApiService {
-  static const String baseUrl = 'http://zabbix.tlconsultorias.com.br:8090';
+  static const String baseUrl = 'http://zabbix.tlconsultorias.com.br:8088';
   static String media(String? path) {
     if (path == null || path.isEmpty) return '';
     if (path.startsWith('http')) return path;
